60a438e2c8b8ea17eafd79528cabc80f0a5380e4
Sayan Nandi <sayannandi11071998@gmail.com>
2025-02-08 11:57:27 +0530

    Sound Service Singleton Applied

--- Assets/Scripts/UI/UIService.cs
+++ Assets/Scripts/UI/UIService.cs
@@ -38,6 +38,21 @@ namespace ServiceLocator.UI
         [SerializeField] private Button playAgainButton;
         [SerializeField] private Button quitButton;
 
+        public static UIService Instance { get { return instance; } }
+        private static UIService instance;
+
+        private void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+            else
+            {
+                Destroy(this.gameObject);
+                Debug.LogError("Singleton of UIService is Trying to create Second Instance");
+            }
+        }
 
         private void Start()
         {
@@ -52,7 +67,7 @@ namespace ServiceLocator.UI
             nextWaveButton.onClick.AddListener(OnNextWaveButton);
             quitButton.onClick.AddListener(OnQuitButtonClicked);
             playAgainButton.onClick.AddListener(OnPlayAgainButtonClicked);
-            
+
             SubscribeToEvents();
         }
 
