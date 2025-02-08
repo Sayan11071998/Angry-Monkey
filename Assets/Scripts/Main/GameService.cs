a99d962093f9e5cbabf11887b0f9d4a55b32d841
Sayan Nandi <sayannandi11071998@gmail.com>
2025-02-09 01:12:53 +0530

    Use Service Locator

--- Assets/Scripts/Main/GameService.cs
+++ Assets/Scripts/Main/GameService.cs
@@ -1,22 +1,50 @@
-using System.Collections;
-using System.Collections.Generic;
-using ServiceLocator.Player;
-using ServiceLocator.Utilities;
 using UnityEngine;
+using ServiceLocator.Utilities;
+using ServiceLocator.Events;
+using ServiceLocator.Map;
+using ServiceLocator.Wave;
+using ServiceLocator.Sound;
+using ServiceLocator.Player;
+using ServiceLocator.UI;
 
-public class GameService : GenericMonoSingleton<GameService>
+namespace ServiceLocator.Main
 {
-    public PlayerService playerService { get; private set; }
-
-    [SerializeField] public PlayerScriptableObject playerScriptableObject;
-
-    private void Start()
+    public class GameService : GenericMonoSingleton<GameService>
     {
-        playerService = new PlayerService(playerScriptableObject);
-    }
+        // Services:
+        public EventService EventService { get; private set; }
+        public MapService MapService { get; private set; }
+        public WaveService WaveService { get; private set; }
+        public SoundService SoundService { get; private set; }
+        public PlayerService PlayerService { get; private set; }
 
-    private void Update()
-    {
-        playerService.Update();
+        [SerializeField] private UIService uiService;
+        public UIService UIService => uiService;
+
+
+        // Scriptable Objects:
+        [SerializeField] private MapScriptableObject mapScriptableObject;
+        [SerializeField] private WaveScriptableObject waveScriptableObject;
+        [SerializeField] private SoundScriptableObject soundScriptableObject;
+        [SerializeField] private PlayerScriptableObject playerScriptableObject;
+
+        // Scene Referneces:
+        [SerializeField] private AudioSource SFXSource;
+        [SerializeField] private AudioSource BGSource;
+
+        private void Start()
+        {
+            EventService = new EventService();
+            UIService.SubscribeToEvents();
+            MapService = new MapService(mapScriptableObject);
+            WaveService = new WaveService(waveScriptableObject);
+            SoundService = new SoundService(soundScriptableObject, SFXSource, BGSource);
+            PlayerService = new PlayerService(playerScriptableObject);
+        }
+
+        private void Update()
+        {
+            PlayerService.Update();
+        }
     }
 }
--- Assets/Scripts/Player/MonkeyController.cs
+++ Assets/Scripts/Player/MonkeyController.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using ServiceLocator.Wave.Bloon;
 using ServiceLocator.Player.Projectile;
-using ServiceLocator.Sound;
+using ServiceLocator.Main;
 
 namespace ServiceLocator.Player
 {
@@ -20,7 +20,7 @@ namespace ServiceLocator.Player
             monkeyView = Object.Instantiate(monkeyScriptableObject.Prefab);
             monkeyView.SetController(this);
             monkeyView.SetTriggerRadius(monkeyScriptableObject.Range);
-
+            
             this.monkeyScriptableObject = monkeyScriptableObject;
             this.projectilePool = projectilePool;
             bloonsInRange = new List<BloonController>();
@@ -28,15 +28,6 @@ namespace ServiceLocator.Player
         }
 
         public void SetPosition(Vector3 positionToSet) => monkeyView.transform.position = positionToSet;
-
-        public void UpdateMonkey()
-        {
-            if (bloonsInRange.Count > 0)
-            {
-                RotateTowardsTarget(bloonsInRange[0]);
-                ShootAtTarget(bloonsInRange[0]);
-            }
-        }
 
         public void BloonEnteredRange(BloonController bloon)
         {
@@ -52,7 +43,14 @@ namespace ServiceLocator.Player
 
         public bool CanAttackBloon(BloonType bloonType) => monkeyScriptableObject.AttackableBloons.Contains(bloonType);
 
-
+        public void UpdateMonkey()
+        {
+            if(bloonsInRange.Count > 0)
+            {
+                RotateTowardsTarget(bloonsInRange[0]);
+                ShootAtTarget(bloonsInRange[0]);
+            }
+        }
 
         private void RotateTowardsTarget(BloonController targetBloon)
         {
@@ -64,12 +62,12 @@ namespace ServiceLocator.Player
         private void ShootAtTarget(BloonController targetBloon)
         {
             attackTimer -= Time.deltaTime;
-            if (attackTimer <= 0)
+            if(attackTimer <= 0)
             {
                 ProjectileController projectile = projectilePool.GetProjectile(monkeyScriptableObject.projectileType);
                 projectile.SetPosition(monkeyView.transform.position);
                 projectile.SetTarget(targetBloon);
-                SoundService.Instance.PlaySoundEffects(Sound.SoundType.MonkeyShoot);
+                GameService.Instance.SoundService.PlaySoundEffects(Sound.SoundType.MonkeyShoot);
                 ResetAttackTimer();
             }
         }
